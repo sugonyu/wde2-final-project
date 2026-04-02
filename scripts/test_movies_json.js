860b71ab0a54353fa84856eccdc16fea1c12a721
Tony Yu <sugonyu@gmail.com>
2026-04-02 13:51:04 -0400

    feat: add movies json mock data

---
+++ scripts/test_movies_json.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+const raw = fs.readFileSync('./data/movies.json', 'utf-8');
+const movies = JSON.parse(raw);
+
+console.log('Movie JSON Test');
+console.log('----------------');
+console.log('Latest movies:', movies.latest.length);
+console.log('Classic movies:', movies.classics.length);
+console.log('Best rated movies:', movies.rated.length);
+console.log('');
+
+console.log('Best Rated Titles:');
+for (let i = 0; i < movies.rated.length; i++) {
+  console.log('-', movies.rated[i].title);
+}
+console.log('');
+
+const allMovies = [...movies.latest, ...movies.classics, ...movies.rated];
+const randomIndex = Math.floor(Math.random() * allMovies.length);
+const randomMovie = allMovies[randomIndex];
+
+console.log('Random Movie Pick:');
+console.log('Title:', randomMovie.title);
+console.log('Genre:', randomMovie.genre);
+console.log('Director:', randomMovie.director);
+console.log('Trailer:', randomMovie.trailer);
